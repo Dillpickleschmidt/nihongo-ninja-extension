7e87b67d16fc135169f0a80c01c0439c4a453207
Dylan Kuzmick <drkuzmick@gmail.com>
2025-10-22 13:42:08 -0400

    fix missing css in ftue-ui.ts

--- extension/src/entrypoints/ftue-ui/ftue-ui.ts
+++ extension/src/entrypoints/ftue-ui/ftue-ui.ts
@@ -1,5 +1,9 @@
 import { renderFtueUi } from '@/ui/ftue';
 import '../video.content/video.css';
+import '@project/common/components/material.css';
+import '@project/common/components/display.css';
+import '@project/common/components/YomitanPopup.css';
+import '@project/common/components/yomitan-structured-content.css';
 import { currentPageDelegate } from '@/services/pages';
 
 window.addEventListener('load', () => {
